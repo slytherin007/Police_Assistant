61d7f4fd9ad704fdb0c53f7e362a362529c25698
Ashwini Bhide <60039504+slytherin007@users.noreply.github.com>
2024-01-25 22:05:01 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,1 +1,15 @@
 # 86e37e2173da469d88e8084f43efa839
+import assemblyai as aai
+
+aai.settings.api_key = "86e37e2173da469d88e8084f43efa839"
+
+audiourl = "audio.mp3"
+
+config = aai.TranscriptionConfig(language_code="hi")
+
+transcriber = aai.Transcriber(config=config)
+
+transcript = transcriber.transcribe(audiourl)
+
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write(transcript.text)
